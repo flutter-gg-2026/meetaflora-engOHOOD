ce67c82f9d325d0d0276ab38b770af488853ea9a
engOHOOD <ohoodalbagami1@gmail.com>
2026-04-12 08:28:45 +0300

    update

---
+++ meet_a_flora/lib/core/theme/cubit/theme_cubit.dart
@@ -0,0 +1,12 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+
+class ThemeCubit extends Cubit<ThemeMode> {
+  ThemeCubit() : super(ThemeMode.light);
+
+
+
+  void switchTheme() {
+    emit(state == ThemeMode.light ? ThemeMode.dark : ThemeMode.light);
+  }
+}
--- meet_a_flora/lib/features/home/data/datasources/home_remote_data_source.dart
+++ meet_a_flora/lib/features/home/data/datasources/home_remote_data_source.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 
 import 'package:flutter/services.dart';
 import 'package:injectable/injectable.dart';
-import 'package:meet_a_flora/core/network/dio_client.dart';
 import 'package:meet_a_flora/features/home/data/models/home_model.dart';
 import 'package:meet_a_flora/core/errors/network_exceptions.dart';
 
@@ -17,17 +16,13 @@ class HomeRemoteDataSource implements BaseHomeRemoteDataSource {
   @override
   Future<List<ImageModel>> getHome() async {
     try {
-      print("---------------------------------3.1");
       final imageJson = await rootBundle.loadString("assets/data/data.json");
-      print("---------------------------------3.3");
 
       final imageList = List.from(
         await jsonDecode(imageJson),
       ).map((img) => ImageModel.fromJson(img)).toList();
-      print("---------------------------------3.4");
       return imageList;
     } catch (error) {
-      print("---------------------------------3.2");
       throw FailureExceptions.getException(error);
     }
   }
